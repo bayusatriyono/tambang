1e3e87d7f9a4d62464a1b286afdae1a2c039f3e2
Bayu Satriyono <bayusatriyono1@gmail.com>
2025-04-14 20:42:46 +0700

    add

--- tugas 2/tugas2.py
+++ tugas 2/tugas2.py
@@ -1,29 +1,14 @@
 import pandas as pd
-import numpy as np
-from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'D:\tambang\tugas 2\train.csv')
-# Mengisi missing values
-imputer_age = SimpleImputer(strategy='median')
-df['Age'] = imputer_age.fit_transform(df[['Age']])
+# Membaca data dari file tv.csv
+data = pd.read_csv(r'D:\tambang\tugas 1\tv.csv')
 
-imputer_embarked = SimpleImputer(strategy='most_frequent')
-df['Embarked'] = imputer_embarked.fit_transform(df[['Embarked']]).ravel()
+# membuat scatter plot dengan lib pandas
+data.plot.scatter(x='av_rating', y='seasonNumber', alpha=0.5)
 
-df['Cabin'] = df['Cabin'].fillna('Unknown')  # Perbaikan inplace=True
-
-# Konversi kategori ke numerik
-df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
-
-# One-Hot Encoding 'Embarked'
-df = pd.get_dummies(df, columns=['Embarked'], drop_first=True)
-
-# Normalisasi 'Age' dan 'Fare'
-df['Age'] = (df['Age'] - df['Age'].min()) / (df['Age'].max() - df['Age'].min())
-df['Fare'] = (df['Fare'] - df['Fare'].min()) / (df['Fare'].max() - df['Fare'].min())
-
-# Hapus kolom yang tidak diperlukan
-df.drop(columns=['PassengerId', 'Name', 'Cabin'], inplace=True)
-
-# Tampilkan hasil akhir
-print(df.head())
+# menampilkan plot
+plt.title("Hubungan antara Peringkat rata-rata dan jumlah musim")
+plt.xlabel("Peringkat rata-rata")
+plt.ylabel("jumlah musim")
+plt.show()
---
+++ tugas 5/tugas5.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Sumber Data
+X = np.array([100, 120, 140, 160, 180, 200, 220, 240, 260, 280]).reshape(-1,1)
+Y = np.array([100, 120, 140, 160, 180, 200, 220, 240, 260, 280])
+
+# Model Regresi
+model = LinearRegression()
+model.fit(X,Y)
+
+# Koefisien
+a = model.intercept_
+b = model.coef_[0]
+
+# Hasil Output Regresi
+print(f"Model regresi: Y = {a:.2f} + {b:.2f} X")
+
+# Hasil Prediksi penjualan
+prediksi = model.predict(np.array([[300]]))
+print(f"Prediksi Penjualan jika anggaran iklan Rp. 300.000 adalah : {int(prediksi[0])} unit")
